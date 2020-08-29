52ef7377618b26735534ac55897aaba0330041a3
mmujadidnaeem <mmujadidnaeem@gmail.com>
2020-08-29 11:15:36 +0500

    Uploaded m6-assignment-gate-a

---
+++ Java-for-android/m6-assignment-gate-a-skeleton/app/src/main/java/mooc/vandy/java4android/gate/logic/Gate.java
@@ -0,0 +1,64 @@
+package mooc.vandy.java4android.gate.logic;
+
+/**
+ * This file defines the Gate class.
+ */
+public class Gate {
+    // TODO -- Fill in your code here
+
+     public static final int IN = 1;
+     public static final int OUT = -1;
+     public static final int CLOSED = 0;
+     private int mSwing;
+
+     public int getSwingDirection(){
+        return  mSwing;
+     }
+
+     public Gate(){
+         mSwing = CLOSED;
+     }
+
+     public boolean open(int direction){
+         if(direction == IN || direction == OUT){
+             mSwing = direction;
+             return true;
+         }
+
+         return false;
+     }
+
+     public void close(){
+         mSwing = 0;
+     }
+
+     public boolean setSwing(int direction){
+         if(direction == IN || direction == OUT){
+             mSwing = direction;
+             return true;
+         }
+             return false;
+     }
+
+    public int thru(int count) {
+        if(mSwing == IN)
+            return count;
+        else if(mSwing == OUT)
+            return - count;
+        else
+            return 0;
+    }
+
+    @Override
+    public String toString() {
+        if(mSwing == CLOSED)
+            return "This gate is closed";
+        else if(mSwing == IN)
+            return "This gate is open and swings to enter the pen only";
+        else if (mSwing == OUT)
+            return "This gate is open and swings to exit the pen only";
+        else
+            return "This gate has an invalid swing direction";
+    }
+
+}
